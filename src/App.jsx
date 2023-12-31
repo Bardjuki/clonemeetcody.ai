fec993dba1e5d1749736960a23ffdc17331b3aab
unknown <ahmadbardjukis@gmail.com>
2023-12-31 07:34:29 +0700

    Added signin and signup form

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import UseCasesPage from './Pages/UseCasesPage'
 import PricingPage from './Pages/PricingPage'
 import BlogPage from './Pages/BlogPage'
 import { Stack } from '@chakra-ui/react'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
 
 function App() {
 
@@ -17,6 +19,8 @@ function App() {
         <Route path='/enterprise' element={<EnterprisePage/>}/>
         <Route path='/pricing' element={<PricingPage/>}/>
         <Route path='/blog' element={<BlogPage/>}/>
+        <Route path='/auth/signin' element={<SignIn/>}/>
+        <Route path='/auth/signup' element={<SignUp/>}/>
       </Routes>
       </Stack>
   )
